6cc32a6ac5dc40b89f1a6693aa2d7f07543b93d7
unknown <C:\Users\user\AppData\Roaming\The Bat!\Mail>
2022-11-09 14:48:44 +1000

    Styled all elements, added password check

---
+++ script.js
@@ -0,0 +1,19 @@
+let passwordForm = document.getElementById('password');
+let confirmationForm = document.getElementById('confirm-password');
+let passwordForms = [passwordForm, confirmationForm]
+let comparison = document.getElementById('password-check');
+
+passwordForms.forEach((el) =>
+    el.addEventListener('input', () => {
+        let password = passwordForm.value;
+        let confirmation = confirmationForm.value;
+
+        if (password.length < 8) {
+            comparison.innerHTML = "* Password must be at least 8 characters long";
+        } else if (password != confirmation) {
+            comparison.innerHTML = "* Passwords do not match";
+        } else if (password == confirmation) {
+            comparison.innerHTML = "* Passwords is correct";
+            comparison.style.color = "blue";
+        }
+}));

98dd336000b581d7b9d9294159d7a8c9b7266204
unknown <C:\Users\user\AppData\Roaming\The Bat!\Mail>
2022-11-10 12:42:04 +1000

    Added styles for mobile

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let passwordForm = document.getElementById('password');
 let confirmationForm = document.getElementById('confirm-password');
 let passwordForms = [passwordForm, confirmationForm]
 let comparison = document.getElementById('password-check');
+let photoCredits = document.querySelector('.photo-credits');
+
+let mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase())); 
 
 passwordForms.forEach((el) =>
     el.addEventListener('input', () => {
@@ -10,10 +13,16 @@ passwordForms.forEach((el) =>
 
         if (password.length < 8) {
             comparison.innerHTML = "* Password must be at least 8 characters long";
+            comparison.style.color = "red";
         } else if (password != confirmation) {
             comparison.innerHTML = "* Passwords do not match";
+            comparison.style.color = "red";
         } else if (password == confirmation) {
             comparison.innerHTML = "* Passwords is correct";
             comparison.style.color = "blue";
         }
 }));
+
+if (mobile) {
+    photoCredits.style.display = "none";
+}
